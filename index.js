88c3745540a9915e5300f01e312604520ae176ab
LiloFr <lilo.frich@gmail.com>
2023-10-30 22:45:21 +0300

    get JWT token and hide passHash from response

--- index.js
+++ index.js
@@ -1,52 +1,69 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
 
-import { validationResult } from 'express-validator';
-import { registerValidation } from './validations/auth.js';
+import UserModel from "./models/User.js";
 
-import UserModel from './models/User.js';
-
-mongoose.connect('mongodb+srv://lilofrich:яоченьсильнодорожуульяной@cluster0.iqo5n9n.mongodb.net/?retryWrites=true&w=majority')
-.then(() => console.log("DB OK"))
-.catch((err) => console.log('DB ERROR', err));
-
+mongoose
+    .connect(
+        "mongodb+srv://lilofrich:яоченьсильнодорожуульяной@cluster0.iqo5n9n.mongodb.net/blog?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("DB OK"))
+    .catch((err) => console.log("DB ERROR", err));
 
 const app = express();
 app.use(express.json());
 
-app.post('/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json(errors.array());
+app.post("/register", registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'secret123',
+        {
+            expiresIn: '30d',
+        });
+        
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } 
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Не удалось зарегистрироваться",
+        });
     }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
-
-    const user = await doc.save();
-
-    res.json(user);
 });
 
-
-
-
-app.listen(4444, (err) =>{
-    if (err){
+app.listen(4444, (err) => {
+    if (err) {
         return console.log(err);
     }
 
-    console.log('Server OK');
+    console.log("Server OK");
 });
-
