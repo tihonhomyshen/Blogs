023cedd52b410df66def79c145a8d5997ed4823d
LiloFr <lilo.frich@gmail.com>
2023-11-01 20:50:07 +0300

    blog model & controller

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -1,7 +1,20 @@
-export const create = (req, res) => {
-    try {
-        
-    } catch (err){ 
+import BlogModel from '../models/Blog'
 
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId,
+        });
+
+        const blog = await doc.save();
+        res.json(blog);
+    } catch (err){ 
+        res.status(500).json({
+            message: 'Не удалось создать статью',
+        });
     }
 }
---
+++ models/Blog.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: Array,
+        default: [],
+    },
+    viewsCount: {
+        type: Number,
+        default: 0,
+    },
+    imageUrl: String,
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model('Blog', BlogSchema);
